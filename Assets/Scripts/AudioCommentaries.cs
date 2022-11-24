9e9a46fd4403b7c4f5e941e0b32d87d32a98d045
honkeypete <30056592@tafe.wa.edu.au>
2022-11-24 11:54:06 +0800

    Alpha/GoldMaster Build

--- Assets/Scripts/AudioCommentaries.cs
+++ Assets/Scripts/AudioCommentaries.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class AudioCommentaries : MonoBehaviour
 {
-    [SerializeField] public AudioSource audioSource { get; private set; }
+    public AudioSource audioSource;
     public AudioClip interactClip;
+    bool isAudioPlaying = false;
 
 
     // Start is called before the first frame update
@@ -17,9 +18,32 @@ public class AudioCommentaries : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Interaction.Instance.CurrentTooltip.TryGetComponent(out AudioSource audioSource) == true)
+        if (Interaction.Instance.CurrentTooltip != null)
         {
-            audioSource.PlayOneShot(interactClip);
+            if (Interaction.Instance.CurrentTooltip.gameObject == this.gameObject)
+            {
+                
+                    if (isAudioPlaying == false)
+                    {
+                        audioSource.PlayOneShot(interactClip);
+                        isAudioPlaying = true;
+                    }
+                
+            }
+            else
+            {
+
+                audioSource.Stop();
+                isAudioPlaying = false;
+
+            }
+        }
+        else
+        {
+
+            audioSource.Stop();
+            isAudioPlaying = false;
+
         }
     }
 }
